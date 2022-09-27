960058936b4ab63060c963e8f535a278d4bbd554
MarcoMoreno1306 <112222778+MarcoMoreno1306@users.noreply.github.com>
2022-09-27 07:39:31 -0500

    Create 5.main.c

---
+++ 5.main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int resultado = 0;
+
+    resultado = div(9,3);
+    printf("El resultado de la division es: %d\n",resultado);
+    resultado = div(8,6);
+    printf("El resultado de la division es: %d\n",resultado);
+    resultado = div(15,5);
+    printf("El resultado de la division es: %d\n",resultado);
+    return 0;
+}
